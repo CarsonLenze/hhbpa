6ba7945452ce9633500a3323e789ace573a3958e
CarsonLenze <carsonlenze@gmail.com>
2023-03-22 13:25:21 -0400

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
 import { NextUIProvider, createTheme } from "@nextui-org/react";
-import "@/styles/globals.css";
-import Head from 'next/head'
 
 const theme = createTheme({
-  type: 'light',
+  type: "light",
   theme: {
     colors: {
       link: "#fff"
@@ -14,19 +12,6 @@ const theme = createTheme({
 function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta charset="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
-        <meta
-          name="description"
-          content="Web site created using create-react-app"
-        />
-        <link rel="apple-touch-icon" href="/logo192.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <title>Hamilton Hights BPA</title>
-      </Head>
       <NextUIProvider theme={theme}>
         <Component {...pageProps} />
       </NextUIProvider>
