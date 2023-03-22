39eb9b57fc020c52b874146307573c305e2884e4
CarsonLenze <carsonlenze@gmail.com>
2023-03-22 11:15:19 -0400

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+import { NextUIProvider } from "@nextui-org/react";
+import "@/styles/globals.css";
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta charset="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Web site created using create-react-app"
+        />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <title>Hamilton Hights BPA</title>
+      </Head>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </>
+  );
 }
+
+export default App;
