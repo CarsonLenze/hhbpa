41f58879d88af897b09694273aba76540256c9d5
CarsonLenze <carsonlenze@gmail.com>
2023-04-12 14:03:58 -0400

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 import { Footer } from '@/components'
+import { useEffect } from 'react'
+import OneSignal from 'react-onesignal';
+//c0046090-9b33-4c39-9197-5120d4f599de
 
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      await OneSignal.init({ appId: 'c0046090-9b33-4c39-9197-5120d4f599de', allowLocalhostAsSecureOrigin: true});
+      OneSignal.showSlidedownPrompt();
+    })();
+}, []); // <-- run this effect once on mount
+
   return (
     <>
       <>Home</>
