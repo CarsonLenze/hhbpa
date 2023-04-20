6c17b7d8db75ef07cbf9bf59666e39d117c8b2c4
CarsonLenze <carsonlenze@gmail.com>
2023-04-20 09:59:46 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import OneSignal from 'react-onesignal';
 export default function Home() {
   useEffect(() => {
     (async () => {
-      await OneSignal.init({ appId: 'c0046090-9b33-4c39-9197-5120d4f599de', allowLocalhostAsSecureOrigin: true});
+      await OneSignal.init({ appId: 'c0046090-9b33-4c39-9197-5120d4f599de' });
       OneSignal.showSlidedownPrompt();
     })();
 }, []); // <-- run this effect once on mount
