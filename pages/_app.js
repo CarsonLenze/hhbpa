bfd3306d931cfb436a0da8583ca7de66cebcced8
CarsonLenze <carsonlenze@gmail.com>
2023-08-17 11:45:50 -0400

    Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+// pages/_app.js
+import {NextUIProvider} from '@nextui-org/react';
+import "../styles/global.css"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
+
+export default MyApp;
